b6487428bf619e8a0429f77ce44b4c24813ccc1b
kuboshu83 <dot.sohkubo@gmail.com>
2024-08-05 05:49:52 +0900

    modalのパッケージインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import TodoList from "./api/top/page";
+import Modal from "react-modal";
 
 export default function Home() {
+  const isOpen = true;
   return (
     <main className="w-full">
+      {/* <Modal isOpen={isOpen}>モーダル開いた</Modal> */}
       <TodoList />
     </main>
   );
