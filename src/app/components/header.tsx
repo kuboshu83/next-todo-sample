77335ce54fcf793ff3fe3e03429476d2d32356ce
kuboshu83 <dot.sohkubo@gmail.com>
2024-08-05 05:50:07 +0900

    todo作成ボタンをヘッダに作成

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,7 +2,10 @@ export const Header: React.FC = () => {
   return (
     <div className="flex justify-between items-center h-full">
       <div className="text-4xl ml-5">TodoApp</div>
-      <div className="text-2xl mr-5">Login</div>
+      <div className="text-2xl mr-5 flex gap-4">
+        <div>NewTodo</div>
+        <div>Login</div>
+      </div>
     </div>
   );
 };
