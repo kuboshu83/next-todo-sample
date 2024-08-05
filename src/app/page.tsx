02ba49b66041465b752fb114a55bf8890dde186c
kuboshu83 <dot.sohkubo@gmail.com>
2024-08-05 21:45:26 +0900

    モーダルのcss調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,24 @@ import Modal from "react-modal";
 import { RootState } from "./store";
 import { AddTodo } from "./api/top/addTodo";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 export default function Home() {
   const isNewTodoEditorEnabled = useSelector(
     (state: RootState) => state.newtodo.enabled
   );
   return (
     <main className="w-full flex">
-      <Modal isOpen={isNewTodoEditorEnabled}>
+      <Modal isOpen={isNewTodoEditorEnabled} style={customStyles}>
         <AddTodo />
       </Modal>
       <TodoList />
